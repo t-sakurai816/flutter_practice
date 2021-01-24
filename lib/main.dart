2df64c7805d0c0ee8daed94ca0257376ffcdc4d5
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-01-24 15:14:47 +0900

    最高にシンプル

--- lib/main.dart
+++ lib/main.dart
@@ -5,47 +5,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'FLutterのサンプル'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.menu), //左側
-        title: Text(widget.title),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              //押したときの処理
-            },
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('タイトルです'),
+        ),
+        body: Container(),
       ),
     );
   }
