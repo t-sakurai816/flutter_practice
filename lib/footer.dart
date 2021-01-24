7ae12f27b1a9f8a3213cf2031cefd324c91e402a
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-01-24 19:20:30 +0900

    forを使ってfooterを作成

--- lib/footer.dart
+++ lib/footer.dart
@@ -8,19 +8,53 @@ class Footer extends StatefulWidget {
 }
 
 class _Footer extends State {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+  //アイコン
+  final Map _footerIcons = {
+    'Me': Icons.person,
+    'Home': Icons.home,
+    'Settings': Icons.settings,
+  };
+
+  //アイコンの名前
+  final List _footerItemOrder = [
+    'Me',
+    'Home',
+    'Settings',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    for (var i = 0; i < _footerItemOrder.length; i++) {
+      _bottomNavigationBarItems.add(createIcon(_footerItemOrder[i]));
+    }
+  }
+
+  // アイコンを作成する
+  BottomNavigationBarItem createIcon(String key) {
+    return BottomNavigationBarItem(
+      icon: Icon(_footerIcons[key]),
+      label: key,
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('home'),
-        ),
-      ],
+      type: BottomNavigationBarType.fixed,
+      items: _bottomNavigationBarItems,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      selectedItemColor: Colors.pinkAccent,
+      unselectedItemColor: Colors.black45,
     );
   }
 }
