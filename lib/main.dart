b798b1889bddb4d9a4731840ea9823619682645c
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-01-24 15:17:16 +0900

    headerを別ファイル別ファイル

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'header.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('タイトルです'),
-        ),
+        appBar: Header(),
         body: Container(),
       ),
     );
