698e7d061c85b1f4d07df3d500a4cf320f291098
t-sakurai816 <53470730+t-sakurai816@users.noreply.github.com>
2021-01-24 17:40:01 +0900

    アイコンを追加

--- lib/header.dart
+++ lib/header.dart
@@ -8,6 +8,17 @@ class Header extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text('ホーム'),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              //押したときの処理
+            },
+          ),
+        )
+      ],
     );
   }
 }
